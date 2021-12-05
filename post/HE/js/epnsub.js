e3122f8493162d944c2595052330187a52781a06
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2021-12-05 17:15:13 +0800

    Add files via upload

---
+++ post/HE/js/epnsub.js
@@ -0,0 +1,14 @@
+function ep02() {
+	    document.getElementById('mainvideo').src='https://hk.91io.cn/s/QX4k5UE/Hawkeye.2021.S01E02.mp4';
+	document.getElementById('mainsub').src='vtt/Hawkeye.2021.S01E02.vtt';
+	}
+
+function ep01() {
+	    document.getElementById('mainvideo').src='https://hk.91io.cn/s/LyVOksK/Hawkeye.2021.S01E01.mp4';
+	document.getElementById('mainsub').src='vtt/Hawkeye.2021.S01E01.vtt';
+	}
+	
+function ep03() {
+	    document.getElementById('mainvideo').src='https://hk.91io.cn/s/5jkpBsk/Hawkeye.2021.S01E03.mp4';
+	document.getElementById('mainsub').src='vtt/Hawkeye.2021.S01E03.vtt';
+	}
