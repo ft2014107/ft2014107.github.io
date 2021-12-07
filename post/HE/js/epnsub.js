400ee5a4a14e8aa6b0b1fca507fe4bc3a7740252
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2021-12-07 21:57:34 +0800

    Add files via upload

--- post/HE/js/epnsub.js
+++ post/HE/js/epnsub.js
@@ -1,14 +1,17 @@
 function ep02() {
 	    document.getElementById('mainvideo').src='https://hk.91io.cn/s/QX4k5UE/Hawkeye.2021.S01E02.mp4';
 	document.getElementById('mainsub').src='vtt/Hawkeye.2021.S01E02.vtt';
+	document.getElementById('titlehe').innerHTML='鹰眼S01E02——2021.12.24过期';
 	}
 
 function ep01() {
 	    document.getElementById('mainvideo').src='https://hk.91io.cn/s/LyVOksK/Hawkeye.2021.S01E01.mp4';
 	document.getElementById('mainsub').src='vtt/Hawkeye.2021.S01E01.vtt';
+	document.getElementById('titlehe').innerHTML='鹰眼S01E01——2021.12.24过期';
 	}
 	
 function ep03() {
 	    document.getElementById('mainvideo').src='https://hk.91io.cn/s/5jkpBsk/Hawkeye.2021.S01E03.mp4';
 	document.getElementById('mainsub').src='vtt/Hawkeye.2021.S01E03.vtt';
+	document.getElementById('titlehe').innerHTML='鹰眼S01E03——2022.01.01过期';
 	}
