7d40528ebf39e2f8d4f03621e8c00170575c156d
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2021-12-15 17:04:11 +0800

    Add files via upload

--- post/HE/js/epnsub.js
+++ post/HE/js/epnsub.js
@@ -2,21 +2,34 @@ function ep02() {
 	    document.getElementById('mainvideo').src='https://hk.91io.cn/s/QX4k5UE/Hawkeye.2021.S01E02.mp4';
 	document.getElementById('mainsub').src='vtt/Hawkeye.2021.S01E02.vtt';
 	document.getElementById('titlehe').innerHTML='鹰眼S01E02——2021.12.24过期';
+	document.getElementById('mainvideo').poster='https://s2.loli.net/2021/12/13/kaXWs7LUcv8bihe.png';
 	}
 
 function ep01() {
 	    document.getElementById('mainvideo').src='https://hk.91io.cn/s/LyVOksK/Hawkeye.2021.S01E01.mp4';
 	document.getElementById('mainsub').src='vtt/Hawkeye.2021.S01E01.vtt';
 	document.getElementById('titlehe').innerHTML='鹰眼S01E01——2021.12.24过期';
+	document.getElementById('mainvideo').poster='https://s2.loli.net/2021/12/13/CeIO9mxkPgN1LBd.png';
 	}
 	
 function ep03() {
 	    document.getElementById('mainvideo').src='https://hk.91io.cn/s/5jkpBsk/Hawkeye.2021.S01E03.mp4';
 	document.getElementById('mainsub').src='vtt/Hawkeye.2021.S01E03.vtt';
 	document.getElementById('titlehe').innerHTML='鹰眼S01E03——2021.12.31过期';
+	document.getElementById('mainvideo').poster='https://s2.loli.net/2021/12/13/Dfb8iHABtj6p9dq.png';
+	
 	}
 function ep04() {
 	    document.getElementById('mainvideo').src='https://hk.91io.cn/s/OWJxaHk/Hawkeye.2021.S01E04.mp4';
 	document.getElementById('mainsub').src='vtt/Hawkeye.2021.S01E04.vtt';
 	document.getElementById('titlehe').innerHTML='鹰眼S01E04——2022.01.07过期';
+	document.getElementById('mainvideo').poster='https://s2.loli.net/2021/12/13/XFn92KxmH5yluOd.png';
+	
 	}
+function ep05() {
+		    document.getElementById('mainvideo').src='https://hk.91io.cn/s/8y1J2fw/Hawkeye.2021.S01E05.mp4';
+		document.getElementById('mainsub').src='vtt/Hawkeye.2021.S01E05.vtt';
+		document.getElementById('titlehe').innerHTML='鹰眼S01E05——2022.01.14过期';
+		document.getElementById('mainvideo').poster='https://pic.imgdb.cn/item/61b9aaa52ab3f51d91550ff6.jpg';
+		
+		}
