031e176416c52fc7152ff770f4ab9ef02ef0b612
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-02-22 11:54:00 +0800

    Add files via upload

--- GLYJJCQ/S1/js/epnsub.js
+++ GLYJJCQ/S1/js/epnsub.js
@@ -6,6 +6,38 @@ function ep01b() {
 			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/xqgLvKhe/tBKQRX6YI-hBZNaGoJuEixu72QoOJaPOVvJx0X7sdbw*';
 			document.getElementById('titlehe').innerHTML='格林一家进城趣 S01E01b 太空飞鸡';
 				}
+function ep02a() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/xqgzA7Fe/IFrzY4YPjxTND33Y3yOPC458EoJcEhEZxo4XqyB5u2Q*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S01E02a 牛排之夜';
+				}
+function ep02b() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/E5rBYri9/SHV1UQ4PBQFpVMVUP-1c1bAPPEikpoXYb4v-oF2LlfE*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S01E02b 蟋蟀的挑战';
+				}
+function ep03a() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/YGMKZxFv/ZzjVEzeFRNdFNe0NxaaCYpP898YPgPw7QdwxXaQZiKk*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S01E03a 游泳池';
+				}
+function ep03b() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/LWrG9wiM/qxUt03Ay54tMid-ZCklm1dtvU0LOZKZYbWdl8dmUrkQ*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S01E03b 提莉的山羊';
+				}
+function ep04a() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/6WoZkXig/YbcgOAUSMyYvtVqcvJKlu15Vnyn25iibgOPpsiZsrDQ*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S01E04a 小球危机';
+				}
+function ep04b() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/Md6nlzcv/y5v6otIgkRenAjxKwBsNizbmb6-ss-DyH0iHcUdX7zw*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S01E04b 浣熊入侵';
+				}
+function ep05a() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/YG6DvLHv/UFhnXabYJUTrajlIONbz2aHkMmC0CVPFWLjw61gX84Q*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S01E05a 奶奶的驾照';
+				}
+function ep05b() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/81yZ3bcW/m-AXYRIkL0d3-Gpf5X-zFBbhyn0-havxbqDD3QjH8yY*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S01E05b 超市丑闻';
+				}
 
 
 
