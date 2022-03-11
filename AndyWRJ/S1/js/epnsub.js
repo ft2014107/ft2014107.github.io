2740f2bd4211640c33567b9ae94f594c68b6aca1
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-03-12 00:56:55 +0800

    Add files via upload

---
+++ AndyWRJ/S1/js/epnsub.js
@@ -0,0 +1,78 @@
+
+function ep01(){
+	document.getElementById('titlehe').innerHTML='安迪沃霍尔时代日记 E01';
+	var url="https://link.jscdn.cn/sharepoint/aHR0cHM6Ly95MmRscS1teS5zaGFyZXBvaW50LmNvbS86djovZy9wZXJzb25hbC9odWlodWlfeTJkbHFfb25taWNyb3NvZnRfY29tL0VhX3RpeGppLWpGRnA3c21vdEVMS3M4QlVWcWNSclJXODR5ZTY2SVNIVEJyS1E_ZT04dTJqZjk.mp4"
+	var id=md5(url);
+	const dp = new DPlayer({container: document.getElementById('dplayer'),
+			video: {url: url,},
+			danmaku: {
+				id: id,
+				api: 'https://dplayer.moerats.com/'
+				}
+			});
+			}
+
+function ep02(){
+	document.getElementById('titlehe').innerHTML='安迪沃霍尔时代日记 E02';
+	var url="https://link.jscdn.cn/sharepoint/aHR0cHM6Ly95MmRscS1teS5zaGFyZXBvaW50LmNvbS86djovZy9wZXJzb25hbC9odWlodWlfeTJkbHFfb25taWNyb3NvZnRfY29tL0VVVGtqWlZMUlE5RGpEMS02Q2JUX2owQmRJcWtsckc3Mmx6VkFhUElSaXJJWFE_ZT1MUHlnZ2M.mp4"
+	var id=md5(url);
+	const dp = new DPlayer({container: document.getElementById('dplayer'),
+			video: {url: url,},
+			danmaku: {
+				id: id,
+				api: 'https://dplayer.moerats.com/'
+				}
+			});
+			}
+
+function ep03(){
+	document.getElementById('titlehe').innerHTML='安迪沃霍尔时代日记 E03';
+	var url="https://link.jscdn.cn/sharepoint/aHR0cHM6Ly95MmRscS1teS5zaGFyZXBvaW50LmNvbS86djovZy9wZXJzb25hbC9odWlodWlfeTJkbHFfb25taWNyb3NvZnRfY29tL0VYalk0OFQ4blB4RGhNNFZLaS1Id1JjQndGVUNJU1RjQURUc3JqXzBJcTBnR3c_ZT1BWnVRZ0Q.mp4"
+	var id=md5(url);
+	const dp = new DPlayer({container: document.getElementById('dplayer'),
+			video: {url: url,},
+			danmaku: {
+				id: id,
+				api: 'https://dplayer.moerats.com/'
+				}
+			});
+			}
+			
+function ep04(){
+	document.getElementById('titlehe').innerHTML='安迪沃霍尔时代日记 E04';
+	var url="https://link.jscdn.cn/sharepoint/aHR0cHM6Ly95MmRscS1teS5zaGFyZXBvaW50LmNvbS86djovZy9wZXJzb25hbC9odWlodWlfeTJkbHFfb25taWNyb3NvZnRfY29tL0VhUDZNajB2dUxKUHVOaU0zejNBdDFFQnhub0d0a1YyUDAxUGxpbU5Najl4blE_ZT0yRXNRYTM.mp4"
+	var id=md5(url);
+	const dp = new DPlayer({container: document.getElementById('dplayer'),
+			video: {url: url,},
+			danmaku: {
+				id: id,
+				api: 'https://dplayer.moerats.com/'
+				}
+			});
+			}
+			
+function ep05(){
+	document.getElementById('titlehe').innerHTML='安迪沃霍尔时代日记 E05';
+	var url="https://link.jscdn.cn/sharepoint/aHR0cHM6Ly95MmRscS1teS5zaGFyZXBvaW50LmNvbS86djovZy9wZXJzb25hbC9odWlodWlfeTJkbHFfb25taWNyb3NvZnRfY29tL0VYLU9KemNIa0xSR2t3S0kxLS04OVhBQjVtSFlFWlR2cm5ON1dFUzRJR2VqcFE_ZT1GZERDR3o.mp4"
+	var id=md5(url);
+	const dp = new DPlayer({container: document.getElementById('dplayer'),
+			video: {url: url,},
+			danmaku: {
+				id: id,
+				api: 'https://dplayer.moerats.com/'
+				}
+			});
+			}
+			
+function ep06(){
+	document.getElementById('titlehe').innerHTML='安迪沃霍尔时代日记 E06';
+	var url="https://link.jscdn.cn/sharepoint/aHR0cHM6Ly95MmRscS1teS5zaGFyZXBvaW50LmNvbS86djovZy9wZXJzb25hbC9odWlodWlfeTJkbHFfb25taWNyb3NvZnRfY29tL0ViT2NxSzFrODN0QXZ1R1lTY2Jjbks4QnBlamIwbmNWTng2T2VudUFUY0txalE_ZT1jM2g0R3c.mp4"
+	var id=md5(url);
+	const dp = new DPlayer({container: document.getElementById('dplayer'),
+			video: {url: url,},
+			danmaku: {
+				id: id,
+				api: 'https://dplayer.moerats.com/'
+				}
+			});
+			}
