7cf64664bd3530f8a8518c1d9fb666a8f4d4afc2
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-03-12 10:54:07 +0800

    Add files via upload

--- Tred/S1/js/epnsub.js
+++ Tred/S1/js/epnsub.js
@@ -1,6 +1,6 @@
 
 function ep01(){
-	var url="https://link.jscdn.cn/sharepoint/aHR0cHM6Ly95MmRscS1teS5zaGFyZXBvaW50LmNvbS86djovZy9wZXJzb25hbC9odWlodWlfeTJkbHFfb25taWNyb3NvZnRfY29tL0ViR2hoOWM0U2VaTnNJWWx5SEhuZlBZQkxkVHl5bU9rMndrQS1UYnpld2toTFE_ZT12WlV3UDY.mp4"
+	var url="https://pan.bilnn.cn/api/v3/file/sourcejump/J8YlMeHz/65oKqLxX9I_E5SZCqWEbiZ54aZpciN3kSnCIecN5cLw*"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
