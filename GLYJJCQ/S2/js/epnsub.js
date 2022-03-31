f353056b6f3664084b90393849d349bbc7e0d38f
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-03-31 09:50:07 +0800

    Add files via upload

--- GLYJJCQ/S2/js/epnsub.js
+++ GLYJJCQ/S2/js/epnsub.js
@@ -18,6 +18,66 @@ function ep05() {
 			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/Xrk1YeUd/vG-XkaxG2ITnA1o5g5SN98G05oTR4UyKXA-Tr0F9UT0*';
 			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E05 计算球_俱乐部';
 				}
+function ep06() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/DoDr7rI9/X4fLEirQnEYEN5dlYbUXulzDwfp0JEUQeanlAB6_cgk*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E06 假冒流行歌手_足球营';
+				}
+function ep07() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/Z9klx2co/RX6Nj8iWWUKy-aGhWUX2drnaqDRCjCf26MRkUBwOA80*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E07 热化了_比尔 外星人';
+				}
+function ep08() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/nO8KgZHe/NkIw0DBLzdVT5MgcWYJmmgICxUm6H2LELEHwk18oIsk*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E08 鲨鱼物件_捕梦网';
+				}
+function ep09() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/pmbDY8Cw/EYw0QpWjeUDlghfB3TDS0YoTNY9B7QfsaSAOEB9tFqs*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E09 沉迷_狂野的一面';
+				}
+function ep10() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/lm57vEfb/TMoCifQNX_h5PBGstv-umIObKjOm19Y5MMNFK2eFImY*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E10 车库故事_动物农场';
+				}
+function ep11() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/lmdegNSb/MvyAcnnx3eSpn44H1TbMCLNTOyvn4kSzXrKHgmlVLZI*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E11 被遗弃_礼物';
+				}
+function ep12() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/GdxZ4kSZ/OtkObmVT5Bla9xEAOmX7YsL4Mirs6X7Y8sbIXKWN3KM*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E12 时间危机_奶奶司机';
+				}
+function ep13() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/J8xkkKFz/IYdt0l5CnAzGULEuSqAP5SMdmOeQCb6-1L0L8Ewmxxc*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E13 提莉风格_我，耕作机器人';
+				}
+function ep14() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/DowWPrt9/WzVV5mU2UDArGAqI0lexcdNvO193dkKR8njklbyinyk*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E14 朋友大会_被骗';
+				}
+function ep15() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/zZ4qJ7sq/UkC_vkk2MDIoIFRWlDh4j-cmcKdrAhtFW2id4RQuYw4*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E15 格林农场的英亩_娃娃玩起来';
+				}
+function ep16() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/OqvD7vIO/OF5U0vhasww8o7SQXutfDRe1AZNt7NPkkr1tj22t0cM*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E16 加布瑞拉的朋友_廉价秀';
+				}
+function ep17() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/AQxW5Gtx/3WlB3d3H_Dbs9BCqIckh5Lkz67WjwGBwLA3LvNiEvnM*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E17 格林镜子_蟋蟀的门票';
+				}
+function ep18() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/WXYJzWH9/FX7YDByl2TGPtnnGpcgaJGZN1jVoU0pOEs2Rsn9pkog*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E18 时代圈_超级奶奶';
+				}
+function ep19() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/2lrewwuG/ILm7EiqHTwQrB3FmJUXF9BDblB1FDfkScuB1_XO48mE*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E19 礼物危机_伤害摩托车';
+				}
+function ep20() {
+			document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://pan.bilnn.cn/api/v3/file/sourcejump/eWzvJpHZ/tP1eM8uQvWP99KDs9mmdPS729i5EE96RNHSzMufemNk*';
+			document.getElementById('titlehe').innerHTML='格林一家进城趣 S02E20 请安静_奇普打击';
+				}
 
 
 
