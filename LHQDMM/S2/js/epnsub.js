89efacfd5abdd2af5bfcabd0ef415051ddb29db7
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-04-12 14:35:20 +0800

    Add files via upload

--- LHQDMM/S2/js/epnsub.js
+++ LHQDMM/S2/js/epnsub.js
@@ -1,18 +1,18 @@
 function ep02() {
-	    document.getElementById('mainvideo').src='https://dwz.ba/2Q0A8';
+	    document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://link.jscdn.cn/sharepoint/aHR0cHM6Ly95MmRscS1teS5zaGFyZXBvaW50LmNvbS86djovZy9wZXJzb25hbC9odWlodWlfeTJkbHFfb25taWNyb3NvZnRfY29tL0VXUExreWdQc2RSS2hmZk1xTVgxZnFzQkRTY1ZwSTFTbEdOMUpnMEptd3hoSHc_ZT1iajhmZUU.jpg';
 	document.getElementById('titlehe').innerHTML='硫磺泉的秘密S02E02——迪幻字幕组';
 	}
 
 function ep01() {
-	    document.getElementById('mainvideo').src='https://dwz.ba/zT98F';
+	    document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://link.jscdn.cn/sharepoint/aHR0cHM6Ly95MmRscS1teS5zaGFyZXBvaW50LmNvbS86djovZy9wZXJzb25hbC9odWlodWlfeTJkbHFfb25taWNyb3NvZnRfY29tL0Vlc1JjOTZDSDFWS2lOeTFSNlJoR1g4QjlVVFBDaWxnOVVKY1lMQmtyQnhIY2c_ZT16N1pVeXM.jpg';
 	document.getElementById('titlehe').innerHTML='硫磺泉的秘密S02E01——迪幻字幕组';
 	}
 function ep03() {
-	    document.getElementById('mainvideo').src='https://dwz.ba/8e8Pq';
+	    document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://link.jscdn.cn/sharepoint/aHR0cHM6Ly95MmRscS1teS5zaGFyZXBvaW50LmNvbS86djovZy9wZXJzb25hbC9odWlodWlfeTJkbHFfb25taWNyb3NvZnRfY29tL0VhN1NfbVI4VWZGTGtLQnpIRUdYdWdJQnVUX1NUUkVXaDZFTDhKQTd0R2JfSlE.jpg';
 	document.getElementById('titlehe').innerHTML='硫磺泉的秘密S02E03——迪幻字幕组';
 	}
 function ep04() {
-	    document.getElementById('mainvideo').src='https://v.ini0.com/player/?url=https://y2dlq-my.sharepoint.com/:v:/g/personal/huihui_y2dlq_onmicrosoft_com/Ed-SiFBsutZGpjdEausuvh0BGrCrPtDfzNRJhs_0avoJ9A?download=1';
+	    document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://y2dlq-my.sharepoint.com/:v:/g/personal/huihui_y2dlq_onmicrosoft_com/Ed-SiFBsutZGpjdEausuvh0BGrCrPtDfzNRJhs_0avoJ9A?download=1';
 	document.getElementById('titlehe').innerHTML='硫磺泉的秘密S02E04——迪幻字幕组';
 	}
 	/*
@@ -34,4 +34,4 @@ function ep08() {
 				}
 
 */
-//播放器地址：https://v.ini0.com/player/?url=
+//播放器地址：https://vip.parwix.com:4433/player/?url=
