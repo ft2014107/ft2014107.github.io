d11da6cc3d33171d6927af02a7c5dbc528e6f31c
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-04-23 11:49:12 +0800

    Add files via upload

--- JP/XRCX/S1/js/epnsub.js
+++ JP/XRCX/S1/js/epnsub.js
@@ -2,6 +2,12 @@ function ep01() {
 			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E5%A4%8F%E6%97%A5%E9%87%8D%E7%8E%B0/%E5%A4%8F%E6%97%A5%E9%87%8D%E7%8E%B0S01E01_x264.mp4';
 			document.getElementById('titlehe').innerHTML='夏日重现 S01E01';
 				}
+function ep02() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E5%A4%8F%E6%97%A5%E9%87%8D%E7%8E%B0/E02.mp4
+';
+			document.getElementById('titlehe').innerHTML='夏日重现 S01E02';
+				}
+
 
 
 
