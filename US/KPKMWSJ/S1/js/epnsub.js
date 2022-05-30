9c48664666cbc6242206672e7870838228f254c7
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-05-30 14:23:19 +0800

    Add files via upload

--- US/KPKMWSJ/S1/js/epnsub.js
+++ US/KPKMWSJ/S1/js/epnsub.js
@@ -3,11 +3,11 @@ function ep01() {
 			document.getElementById('titlehe').innerHTML='库普和卡米问世界 S01E01';
 				}
 function ep02() {
-			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=';
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E5%BA%93%E6%99%AE%E5%92%8C%E5%8D%A1%E7%B1%B3%E9%97%AE%E4%B8%96%E7%95%8C/S1/S1%E6%AD%A3%E5%BC%8F%E7%89%88/Coop.And.Cami.Ask.The.World.S01E02.720p.DSNP.WEB-DL.mp4';
 			document.getElementById('titlehe').innerHTML='库普和卡米问世界 S01E02';
 				}
 function ep03() {
-			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=';
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E5%BA%93%E6%99%AE%E5%92%8C%E5%8D%A1%E7%B1%B3%E9%97%AE%E4%B8%96%E7%95%8C/S1/S1%E6%AD%A3%E5%BC%8F%E7%89%88/Coop.And.Cami.Ask.The.World.S01E03.720p.DSNP.WEB-DL.mp4';
 			document.getElementById('titlehe').innerHTML='库普和卡米问世界 S01E03';
 				}
 
