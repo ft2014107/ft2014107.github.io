ec5a0d99ea8e6b2ec5c5d2ccd2b50b4699a7f1a4
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-06-19 17:25:16 +0800

    Add files via upload

---
+++ US/DEADEND/S1/js/epnsub.js
@@ -0,0 +1,23 @@
+function ep01() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://vd2.bdstatic.com/mda-nfi81n9gg7jwxhrd/720p/h264/1655617874716084649/mda-nfi81n9gg7jwxhrd.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E01';
+				}
+
+
+
+
+
+
+
+
+
+
+
+
+
+				/*模板：
+function ep() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E';
+				}
+				*/
