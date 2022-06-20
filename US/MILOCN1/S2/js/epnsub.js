c66867d1137111611e16cafa99d7d2107182f579
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-06-20 13:51:28 +0800

    Add files via upload

---
+++ US/MILOCN1/S2/js/epnsub.js
@@ -0,0 +1,57 @@
+function ep01() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E7%B1%B3%E7%BD%97%E5%A2%A8%E8%8F%B2%E5%AE%9A%E5%BE%8B/%E5%9B%BD%E9%85%8DS2/S02E01.mkv';
+			document.getElementById('titlehe').innerHTML='米罗墨菲定律 S02E01';
+				}
+				
+function ep02() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://od.bodence.top/api/name/S02E02.mp4/?path=/%E7%B1%B3%E7%BD%97%E5%A2%A8%E8%8F%B2%E5%AE%9A%E5%BE%8B/%E5%9B%BD%E9%85%8DS2/S02E02.mkv';
+			document.getElementById('titlehe').innerHTML='米罗墨菲定律 S02E02';
+				}
+				
+function ep03() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://od.bodence.top/api/name/S02E03.mp4/?path=/%E7%B1%B3%E7%BD%97%E5%A2%A8%E8%8F%B2%E5%AE%9A%E5%BE%8B/%E5%9B%BD%E9%85%8DS2/S02E03.mkv';
+			document.getElementById('titlehe').innerHTML='米罗墨菲定律 S02E03';
+				}
+				
+function ep04() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://od.bodence.top/api/name/S02E04.mp4/?path=/%E7%B1%B3%E7%BD%97%E5%A2%A8%E8%8F%B2%E5%AE%9A%E5%BE%8B/%E5%9B%BD%E9%85%8DS2/S02E04.mkv';
+			document.getElementById('titlehe').innerHTML='米罗墨菲定律 S02E04';
+				}
+				
+function ep05() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://od.bodence.top/api/name/S02E05.mp4/?path=/%E7%B1%B3%E7%BD%97%E5%A2%A8%E8%8F%B2%E5%AE%9A%E5%BE%8B/%E5%9B%BD%E9%85%8DS2/S02E05.mkv';
+			document.getElementById('titlehe').innerHTML='米罗墨菲定律 S02E05';
+				}
+				
+function ep06() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://od.bodence.top/api/name/S02E06.mp4/?path=/%E7%B1%B3%E7%BD%97%E5%A2%A8%E8%8F%B2%E5%AE%9A%E5%BE%8B/%E5%9B%BD%E9%85%8DS2/S02E06.mkv';
+			document.getElementById('titlehe').innerHTML='米罗墨菲定律 S02E06';
+				}
+				
+function ep07() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://od.bodence.top/api/name/S02E07.mp4/?path=/%E7%B1%B3%E7%BD%97%E5%A2%A8%E8%8F%B2%E5%AE%9A%E5%BE%8B/%E5%9B%BD%E9%85%8DS2/S02E07.mkv';
+			document.getElementById('titlehe').innerHTML='米罗墨菲定律 S02E07';
+				}
+				
+function ep08() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://od.bodence.top/api/name/S02E08.mp4/?path=/%E7%B1%B3%E7%BD%97%E5%A2%A8%E8%8F%B2%E5%AE%9A%E5%BE%8B/%E5%9B%BD%E9%85%8DS2/S02E08.mkv';
+			document.getElementById('titlehe').innerHTML='米罗墨菲定律 S02E08';
+				}
+
+
+
+
+
+
+
+
+
+
+
+
+				/*模板：
+function ep() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=';
+			document.getElementById('titlehe').innerHTML='米罗墨菲定律 S02E';
+				}
+				*/
