0d088d714bd44a6e900088ae5d93dc57d0989e69
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-06-21 11:55:52 +0800

    Add files via upload

--- US/DEADEND/S1/js/epnsub.js
+++ US/DEADEND/S1/js/epnsub.js
@@ -2,9 +2,14 @@ function ep01() {
 			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://vd2.bdstatic.com/mda-nfi81n9gg7jwxhrd/720p/h264/1655617874716084649/mda-nfi81n9gg7jwxhrd.mp4';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E01';
 				}
-
-
-
+function ep02() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://vd3.bdstatic.com/mda-nfjf703is5vjt81c/720p/h264/1655722033286961121/mda-nfjf703is5vjt81c.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E02';
+				}
+function ep03() {
+			document.getElementById('mainvideo').src='https://vip.parwix.com:4433/player/?url=https://f.video.weibocdn.com/u0/kVN3Ta6cgx07X11xu60o0104120ecflS0E060.mp4?label=mp4_1080p&amp;template=1920x1080.25.0&amp;media_id=4782759420559449&amp;tp=8x8A3El:YTkl0eM8&amp;us=0&amp;ori=1&amp;bf=2&amp;ot=h&amp;ps=3lckmu&amp;uid=684C0R&amp;ab=7397-g1,6377-g0,8013-g0,3601-g28,1258-g0,7598-g0&amp;Expires=1655786772&amp;ssig=NKQ4mC%2FKe%2F&amp;KID=unistore,video';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E03';
+				}
 
 
 
