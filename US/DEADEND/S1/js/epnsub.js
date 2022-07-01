c92bfde963623d9cf1ae16fb050bea8a0372d8b3
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-07-02 07:49:22 +0800

    Add files via upload

--- US/DEADEND/S1/js/epnsub.js
+++ US/DEADEND/S1/js/epnsub.js
@@ -18,6 +18,23 @@ function ep05() {
 			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/05.mp4';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E05';
 				}
+function ep06() {
+			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/06.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E06';
+				}
+function ep07() {
+			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/07.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E07';
+				}
+function ep08() {
+			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/08.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E08';
+				}
+function ep09() {
+			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/09.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E09';
+				}
+
 
 
 
