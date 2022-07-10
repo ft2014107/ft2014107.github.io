db46a2244a95ac79823466471c8f3fe1e6509347
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-07-10 20:17:35 +0800

    Add files via upload

--- US/DEADEND/S1/js/epnsub.js
+++ US/DEADEND/S1/js/epnsub.js
@@ -1,41 +1,41 @@
 function ep01() {
-			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://vd2.bdstatic.com/mda-nfi81n9gg7jwxhrd/720p/h264/1655617874716084649/mda-nfi81n9gg7jwxhrd.mp4';
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://vd2.bdstatic.com/mda-nfi81n9gg7jwxhrd/720p/h264/1655617874716084649/mda-nfi81n9gg7jwxhrd.mp4';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E01';
 				}
 function ep02() {
-			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://vd3.bdstatic.com/mda-nfjf703is5vjt81c/720p/h264/1655722033286961121/mda-nfjf703is5vjt81c.mp4';
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://vd3.bdstatic.com/mda-nfjf703is5vjt81c/720p/h264/1655722033286961121/mda-nfjf703is5vjt81c.mp4';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E02';
 				}
 function ep03() {
-			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/03.mp4';
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/03.mp4';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E03';
 				}
 function ep04() {
-			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/04.mp4';
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/04.mp4';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E04';
 				}
 function ep05() {
-			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/05.mp4';
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/05.mp4';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E05';
 				}
 function ep06() {
-			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/06.mp4';
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/06.mp4';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E06';
 				}
 function ep07() {
-			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/07.mp4';
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/07.mp4';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E07';
 				}
 function ep08() {
-			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/08.mp4';
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/08.mp4';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E08';
 				}
 function ep09() {
-			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/09.mp4';
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/09.mp4';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E09';
 				}
 function ep10() {
-			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/10.mp4';
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/10.mp4';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E10';
 				}
 
@@ -48,7 +48,7 @@ function ep10() {
 
 				/*模板：
 function ep() {
-			document.getElementById('mainvideo').src='https://jx.parwix.com:4433/player/?url=';
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=';
 			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E';
 				}
 				*/
