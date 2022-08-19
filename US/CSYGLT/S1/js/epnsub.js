274119b743452b786458d05af0e37294f4e508ec
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-08-19 21:57:08 +0800

    Add files via upload

---
+++ US/CSYGLT/S1/js/epnsub.js
@@ -0,0 +1,20 @@
+
+function ep01() {
+	    document.getElementById('mainvideo').src='https://od.bodence.top/US/%E4%BB%93%E9%BC%A0%E4%B8%8E%E6%A0%BC%E8%95%BE%E7%89%B9/S1/01.mp4';
+	document.getElementById('titlehe').innerHTML='仓鼠与格蕾特S01E01';
+	}
+
+
+
+
+
+
+
+
+
+	/*
+function ep() {
+	    document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=';
+	document.getElementById('titlehe').innerHTML='仓鼠与格蕾特S01E';
+	}
+	*/
