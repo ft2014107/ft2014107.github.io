a6a2cfb8721930a7a5cdd5954440186a127a2eac
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2022-09-13 09:33:13 +0800

    Add files via upload

--- JP/KILLLAKILL/S1/js/epnsub.js
+++ JP/KILLLAKILL/S1/js/epnsub.js
@@ -38,12 +38,66 @@ function ep10() {
 			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/10.mp4';
 			document.getElementById('titlehe').innerHTML='斩服少女 S01E10';
 				}
-
-
-
-
-
-
+function ep11() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/11.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E11';
+				}
+function ep12() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/12.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E12';
+				}
+function ep13() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/13.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E13';
+				}
+function ep14() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/14.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E14';
+				}
+function ep15() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/15.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E15';
+				}
+function ep16() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/16.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E16';
+				}
+function ep17() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/17.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E17';
+				}
+function ep18() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/18.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E18';
+				}
+function ep19() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/19.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E19';
+				}
+function ep20() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/20.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E20';
+				}
+function ep21() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/21.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E21';
+				}
+function ep22() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/22.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E22';
+				}
+function ep23() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/23.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E23';
+				}
+function ep24() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/24.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E24';
+				}
+function ep25() {
+			document.getElementById('mainvideo').src='https://jx.bozrc.com:4433/player/?url=https://od.bodence.top/api/raw/?path=/%E6%97%A5%E6%BC%AB/%E6%96%A9%E6%9C%8D%E5%B0%91%E5%A5%B3/25.mp4';
+			document.getElementById('titlehe').innerHTML='斩服少女 S01E25';
+				}
 
 
 
