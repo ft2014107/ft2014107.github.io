bb3feacafd5a059cc3ebd7da50fa5d95b761080b
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2023-02-08 19:28:39 +0800

    Add files via upload

--- US/SW/S1/js/epnsub.js
+++ US/SW/S1/js/epnsub.js
@@ -1,6 +1,6 @@
 
 function ep01() {
-	    document.getElementById('mainvideo').src='https://www.8090g.cn/?url=https://vd4.bdstatic.com/mda-nd5pszuk1me913f5/720p/h264_delogo/1649264030479363028/mda-nd5pszuk1me913f5.mp4';
+	    document.getElementById('mainvideo').src='https://jx.playerjy.com/?url=https://vd4.bdstatic.com/mda-nd5pszuk1me913f5/720p/h264_delogo/1649264030479363028/mda-nd5pszuk1me913f5.mp4';
 	document.getElementById('titlehe').innerHTML='超自然奇遇S01E01';
 	}
 
@@ -14,7 +14,7 @@ function ep01() {
 
 	/*
 function ep() {
-	    document.getElementById('mainvideo').src='https://www.8090g.cn/?url=';
+	    document.getElementById('mainvideo').src='https://jx.playerjy.com/?url=';
 	document.getElementById('titlehe').innerHTML='超自然奇遇S01';
 	}
 	*/
