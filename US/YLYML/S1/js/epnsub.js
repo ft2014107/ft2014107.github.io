61c32609e2fdd1a40b1017f78461bfe1ed8d0d65
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2023-06-30 01:59:27 +0800

    Update epnsub.js

--- US/YLYML/S1/js/epnsub.js
+++ US/YLYML/S1/js/epnsub.js
@@ -1,6 +1,6 @@
 function ep01() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E01——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/01.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/01.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -9,7 +9,7 @@ function ep01() {
 
 function ep02() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E02——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/02.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/02.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -18,7 +18,7 @@ function ep02() {
 	
 function ep03() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E03——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/03.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/03.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -27,7 +27,7 @@ function ep03() {
 	
 function ep04() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E01——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/04.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/04.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -36,7 +36,7 @@ function ep04() {
 	
 function ep05() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E01——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/05.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/05.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -45,7 +45,7 @@ function ep05() {
 	
 function ep06() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E06——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/06.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/06.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -54,7 +54,7 @@ function ep06() {
 	
 function ep07() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E07——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/07.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/07.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -63,7 +63,7 @@ function ep07() {
 	
 function ep08() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E08——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/08.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/08.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -72,7 +72,7 @@ function ep08() {
 	
 function ep09() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E09——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/09.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/09.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -81,7 +81,7 @@ function ep09() {
 
 function ep10() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E10——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/10.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/10.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -90,7 +90,7 @@ function ep10() {
 
 function ep11() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E11——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/11.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/11.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -99,7 +99,7 @@ function ep11() {
 
 function ep12() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E12——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/12.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/12.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -108,7 +108,7 @@ function ep12() {
 	
 function ep13() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E13——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/13.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/13.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -117,7 +117,7 @@ function ep13() {
 	
 function ep14() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E14——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/14.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/14.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -126,7 +126,7 @@ function ep14() {
 	
 function ep15() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E15——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/15.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/15.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -135,7 +135,7 @@ function ep15() {
 	
 function ep16() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E16——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/16.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/16.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -144,7 +144,7 @@ function ep16() {
 	
 function ep17() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E17——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/17.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/17.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -153,7 +153,7 @@ function ep17() {
 	
 function ep18() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E18——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/18.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/18.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
@@ -162,7 +162,7 @@ function ep18() {
 	
 function ep19() {
 			document.getElementById('titlehe').innerHTML='幽灵与莫莉S01E19——迪幻字幕组';
-	var url="https://od.bodence.top/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/19.mp4"
+	var url="https://od.bodence.top/api/raw/?path=/%E8%BF%AA%E5%B9%BB/%E5%B9%BD%E7%81%B5%E4%B8%8E%E8%8E%AB%E8%8E%89/S1/19.mp4"
 	var id=md5(url);
 	const dp = new DPlayer({container: document.getElementById('dplayer'),
 			video: {url: url,},
