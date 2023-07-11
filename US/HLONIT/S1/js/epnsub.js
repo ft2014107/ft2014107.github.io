d6bcc32d52c08cf9008faacaad75cbbd78a5c917
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2023-07-12 03:27:01 +0800

    Add files via upload

--- US/HLONIT/S1/js/epnsub.js
+++ US/HLONIT/S1/js/epnsub.js
@@ -1,42 +1,42 @@
 function ep01() {
 			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd2.bdstatic.com/mda-nfi81n9gg7jwxhrd/720p/h264/1655617874716084649/mda-nfi81n9gg7jwxhrd.mp4';
-			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E01';
+			document.getElementById('titlehe').innerHTML='海莉救世界S01E01——迪幻字幕组';
 				}
 function ep02() {
 			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd3.bdstatic.com/mda-nfjf703is5vjt81c/720p/h264/1655722033286961121/mda-nfjf703is5vjt81c.mp4';
-			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E02';
+			document.getElementById('titlehe').innerHTML='海莉救世界S01E02——迪幻字幕组';
 				}
 function ep03() {
 			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd3.bdstatic.com/mda-pfa9y5hhkphwpbgm/720p/h264/1686467105755256407/mda-pfa9y5hhkphwpbgm.mp4';
-			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E03';
+			document.getElementById('titlehe').innerHTML='海莉救世界S01E03——迪幻字幕组';
 				}
 function ep04() {
 			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd4.bdstatic.com/mda-pfbfitmn7nu56k7y/720p/h264/1686567765006156064/mda-pfbfitmn7nu56k7y.mp4';
-			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E04';
+			document.getElementById('titlehe').innerHTML='海莉救世界S01E04——迪幻字幕组';
 				}
 function ep05() {
 			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd2.bdstatic.com/mda-pfcfctf81uvdza8y/720p/h264/1686654665874788317/mda-pfcfctf81uvdza8y.mp4';
-			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E05';
+			document.getElementById('titlehe').innerHTML='海莉救世界S01E05——迪幻字幕组';
 				}
 function ep06() {
 			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd2.bdstatic.com/mda-pfd7egnbb2rpsafn/720p/h264/1686719915325405247/mda-pfd7egnbb2rpsafn.mp4';
-			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E06';
+			document.getElementById('titlehe').innerHTML='海莉救世界S01E06——迪幻字幕组';
 				}
 function ep07() {
 			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd2.bdstatic.com/mda-pfe6ey4581azrfqs/720p/h264/1686803761626328046/mda-pfe6ey4581azrfqs.mp4';
-			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E07';
+			document.getElementById('titlehe').innerHTML='海莉救世界S01E07——迪幻字幕组';
 				}
 function ep08() {
 			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd3.bdstatic.com/mda-pff91kjx3tii9agr/720p/h264/1686902767033135184/mda-pff91kjx3tii9agr.mp4';
-			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E08';
+			document.getElementById('titlehe').innerHTML='海莉救世界S01E08——迪幻字幕组';
 				}
 function ep09() {
-			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd3.bdstatic.com/mda-pfh90d4ccgug9x2j/720p/h264/1687069610670272920/mda-pfh90d4ccgug9x2j.mp4';
-			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E09';
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/S1/09.mp4';
+			document.getElementById('titlehe').innerHTML='海莉救世界S01E09——迪幻字幕组';
 				}
 function ep10() {
-			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd4.bdstatic.com/mda-pfiddhs3c1u1xgk6/720p/h264/1687167521515013021/mda-pfiddhs3c1u1xgk6.mp4';
-			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E10';
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://od.bodence.top/api/raw/?path=/US/%E7%A9%B7%E9%80%94%E6%9C%AB%E8%B7%AF%EF%BC%9A%E7%81%B5%E5%BC%82%E6%B8%B8%E4%B9%90%E5%9B%AD/S1/10.mp4';
+			document.getElementById('titlehe').innerHTML='海莉救世界S01E10——迪幻字幕组';
 				}
 
 
@@ -49,6 +49,6 @@ function ep10() {
 				/*模板：
 function ep() {
 			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=';
-			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S01E';
+			document.getElementById('titlehe').innerHTML='海莉救世界S01E';
 				}
 				*/
