894fa9ef771a8bbfe13910dd9cbd3d84a65d7d31
ft2014107 <56401210+ft2014107@users.noreply.github.com>
2024-11-25 17:58:35 +0800

    Add files via upload

---
+++ US/DEADEND/S2/js/epnsub.js
@@ -0,0 +1,54 @@
+function ep01() {
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd2.bdstatic.com/mda-phijfbvteh41famv/720p/h264/1692452888172217173/mda-phijfbvteh41famv.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S02E01';
+				}
+function ep02() {
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd3.bdstatic.com/mda-nfjf703is5vjt81c/720p/h264/1655722033286961121/mda-nfjf703is5vjt81c.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S02E02';
+				}
+function ep03() {
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd3.bdstatic.com/mda-pfa9y5hhkphwpbgm/720p/h264/1686467105755256407/mda-pfa9y5hhkphwpbgm.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S02E03';
+				}
+function ep04() {
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd4.bdstatic.com/mda-pfbfitmn7nu56k7y/720p/h264/1686567765006156064/mda-pfbfitmn7nu56k7y.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S02E04';
+				}
+function ep05() {
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd2.bdstatic.com/mda-pfcfctf81uvdza8y/720p/h264/1686654665874788317/mda-pfcfctf81uvdza8y.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S02E05';
+				}
+function ep06() {
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd2.bdstatic.com/mda-pfd7egnbb2rpsafn/720p/h264/1686719915325405247/mda-pfd7egnbb2rpsafn.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S02E06';
+				}
+function ep07() {
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd2.bdstatic.com/mda-pfe6ey4581azrfqs/720p/h264/1686803761626328046/mda-pfe6ey4581azrfqs.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S02E07';
+				}
+function ep08() {
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd3.bdstatic.com/mda-pff91kjx3tii9agr/720p/h264/1686902767033135184/mda-pff91kjx3tii9agr.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S02E08';
+				}
+function ep09() {
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd3.bdstatic.com/mda-pfh90d4ccgug9x2j/720p/h264/1687069610670272920/mda-pfh90d4ccgug9x2j.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S02E09';
+				}
+function ep10() {
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=https://vd4.bdstatic.com/mda-pfiddhs3c1u1xgk6/720p/h264/1687167521515013021/mda-pfiddhs3c1u1xgk6.mp4';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S02E10';
+				}
+
+
+
+
+
+
+
+
+				/*模板：
+function ep() {
+			document.getElementById('mainvideo').src='https://jx.xmflv.com/?url=';
+			document.getElementById('titlehe').innerHTML='穷途末路：灵异游乐园 S02E';
+				}
+				*/
